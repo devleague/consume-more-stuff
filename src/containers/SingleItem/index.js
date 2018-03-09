3e60b6b1bd0a0c6f76bef2ddf1dbae74e54811e9
KanKan <cindykan@KanKan.local>
2018-03-08 21:06:46 -1000

    majority of item-detail-view complete; need minor adjustments

--- src/containers/SingleItem/index.js
+++ src/containers/SingleItem/index.js
@@ -38,21 +38,170 @@ class SingleItem extends Component {
  
     if (loggedIn) {
       return (
-        <div className="detail-view">
-          <h2>Item Detail View</h2>
-          {
-            isAuthorized(loggedIn.id, userId) ? <Link to={`edit-item/${this.props.match.params.id}`}>EDIT ITEM</Link>
-              :null
-          }
-          <Item {...item}/>
+        <div className="main-content">
+
+          <div className="item-detail-category-row">
+            <div className="category-title">
+              <h2><Item category={this.state.category}/></h2>
+            </div>
+            <div className="page-options-container">
+              <div className="page-options">
+                <div className="icon">
+                  <i className="fas fa-print"></i>
+                </div>
+                <div className="icon">
+                  <i className="far fa-heart"></i>
+                </div>
+              </div>
+              <div className="report-button-container">
+                <button>Report</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="detail-view-container">
+            <figure className="item-detail-image">
+              <img src="https://picsum.photos/300/?random" alt="Item image"/>
+            </figure>
+
+            <div className="item-detail-container">
+              <div className="item-title-row">
+                <h1><Item name={this.state.name}/></h1>
+              </div>
+
+              <div className="item-timestamp-row">
+                <Item created_at={this.state.created_at}/>
+                <Item updated_at={this.state.updated_at}/>
+              </div>
+
+              <div className="item-price-row">
+                <div className="stars-row">
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="far fa-star"></i>
+                  <i className="far fa-star"></i>
+                </div>
+                <div className="item-price">
+                  <h2><Item price={this.state.price}/></h2>
+                </div>
+              </div>
+
+              <div className="item-description-row">
+                <Item className="item-description" notes={this.state.notes}/>
+              </div>
+
+              <div className="item-owner-row">
+                <Item className="item-owner" user={this.state.user}/>
+                {
+                  isAuthorized(loggedIn.id, userId) 
+                  ? 
+                  <Link to={`edit-item/${this.props.match.params.id}`}>EDIT ITEM</Link>
+                  :null
+                }
+                {
+                  isAuthorized(loggedIn.id, userId) 
+                  ? 
+                  <Item className="item-status" itemStatus={this.state.itemStatus}/>
+                  :null
+                }
+              </div>
+
+              <div className="item-specs-row">
+                <Item className="item-condition" conditions={this.state.condition}/>
+                <Item className="item-model" model={this.state.model}/>
+                <Item className="item-dimensions" dimensions={this.state.dimensions}/>
+              </div>
+              
+              <div className="item-button-row">
+                <div className="add-to-cart-button-container">
+                  <button>Add to Cart</button>
+                </div>
+              </div>
+
+              <div className="item-detail-footer">
+              
+                <div className="icon">
+                  <i className="fab fa-facebook-square"></i>
+                </div>
+                <div className="icon">
+                  <i className="fab fa-google-plus-square"></i>
+                </div>
+                <div className="icon">
+                  <i className="fab fa-twitter-square"></i>
+                </div>
+              </div>
+            </div>
+          
+          </div>
         </div>
       )
     } else {
       return (
-      <div className="detail-view">
-          <h2>Item Detail View</h2>
-          <Item {...item}/>
+        <div className="main-content">
+
+          <div className="item-detail-category-row">
+            <div className="category-title">
+              <h2><Item category={this.state.category}/></h2>
+            </div>
+            <div className="page-options"></div>
+              <i class="fas fa-print"></i>
+              <i class="far fa-heart"></i>
+              <button>Report</button>
+          </div>
+
+          <div className="detail-view-container">
+            <figure>
+              <img src="https://picsum.photos/200/?random" alt="Item image"/>
+            </figure>
+
+            <div className="item-title-row">
+              <h1><Item name={this.state.name}/></h1>
+            </div>
+
+            <div className="item-timestamp-row">
+              <Item created_at={this.state.created_at}/>
+              <Item updated_at={this.state.updated_at}/>
+            </div>
+
+            <div className="item-price-row">
+              <div className="stars-row">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+              </div>
+              <div className="item-price"></div>
+                <h2><Item price={this.state.price}/></h2>
+            </div>
+
+            <div className="item-description-row">
+              <Item className="item-description" notes={this.state.notes}/>
+            </div>
+
+            <div className="item-owner-row">
+              <Item className="item-owner" user={this.state.user}/>
+            </div>
+
+            <div className="item-specs-row">
+              <Item className="item-condition" conditions={this.state.condition}/>
+              <Item className="item-model" model={this.state.model}/>
+              <Item className="item-dimensions" dimensions={this.state.dimensions}/>
+            </div>
+
+            <button className="add-to-cart">Add to Cart</button>
+
+            <div className="item-detail-footer">
+              <i class="fab fa-facebook-square"></i>
+              <i class="fab fa-google-plus-square"></i>
+              <i class="fab fa-twitter-square"></i>
+            </div>
+
+          </div>
         </div>
+
+
         )
     }
   }
